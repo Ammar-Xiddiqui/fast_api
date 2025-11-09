fdfbf85008e1a1179e1459548d58a165159f7cf4
ammar <ammarsiddiqui377@gmail.com>
2025-11-09 22:55:42 +0500

    jinja2 templating added

---
+++ jinja-templating/main.py
@@ -0,0 +1,21 @@
+from fastapi import FastAPI,Request
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+
+
+app=FastAPI()
+app.mount("/static",StaticFiles(directory="static"),name="static")
+templates=Jinja2Templates(directory="templates")
+
+list=[
+    
+    {'name':'ammar','profession':'data enginner'},
+    {'name':'sammar','profession':'hr manager'},
+      
+      
+      ]
+
+
+@app.get("/")
+def name(request:Request):
+    return templates.TemplateResponse("index.html",{'request':request,'name':'ammmar','lists':list})
