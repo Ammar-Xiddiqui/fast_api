0604bcdf84d4f098837319775e59eca24ccb8b96
ammar <ammarsiddiqui377@gmail.com>
2025-11-10 13:55:23 +0500

    react practicing

---
+++ react/01_folder/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const App = () => {
+  return (
+<>
+<div>hello bro </div>
+<h1>hello helooo</h1>
+
+</>
+  )
+}
+
+export default App
---
+++ react/prac/2.js
@@ -0,0 +1,3 @@
+import b from './1.js'
+import {a} from './1.js'
+console.log(a,b)
